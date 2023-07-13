4840803435ca8c3820414b4b0a325639366fc2f5
DavidMai <diveintoscience123@gmail.com>
2023-07-13 17:15:36 +0700

    first commit

---
+++ src/store/store.js
@@ -0,0 +1,43 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+    strict: true,
+    state: {
+        products: [
+            {name: 'Banana Skin', price: 20},
+            {name: 'Shiny Star', price: 40},
+            {name: 'Green Shells', price: 60},
+            {name: 'Red Shells', price: 80}
+        ]
+    },
+    getters: {
+        saleProducts: (state) => {
+            var saleProducts = state.products.map( product => {
+                return {
+                    name:  '**' + product.name + '**',
+                    price: product.price / 2,
+                };
+            });
+            return saleProducts;
+        }
+    },
+    mutations: {
+        reducePrice: (state, payload) => {
+            state.products.forEach( product => {
+                console.log('okok')
+                product.price -= payload
+            });
+        }
+    },
+    actions: {
+        reducePrice: (context, payload) => {
+            setTimeout(function(){ // reach out for data
+                console.log('okok')
+                context.commit('reducePrice', payload);
+            }, 2000);
+        }
+    }
+});
